16b4625a68737f307f861c60fa1b40343c9dbba5
CindhaRizkiana <128722737+CindhaRizkiana@users.noreply.github.com>
2023-09-11 13:35:17 +0800

    Add files via upload
    API for a sentiment analysis machine learning model involves creating a program that allows other software to send text data for sentiment analysis and receive analysis results. Key steps include selecting and training the model, using a framework like Flask to develop the API, preprocessing text, making predictions, documenting the API, testing, deploying, and ongoing maintenance. Integrating Swagger UI with Flask adds a user-friendly interface for understanding and testing the API, simplifying documentation and usability. This combination enhances the accessibility and adoption of the sentiment analysis API by developers and clients.

---
+++ API_code/flask_swagger.py
@@ -0,0 +1,180 @@
+import pandas as pd
+from flask import Flask, request, jsonify, make_response, Response, render_template
+from flask_swagger_ui import get_swaggerui_blueprint
+from sentimentprediction import sentiment_text, sentiment_file
+
+# Init app
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False  #Agar Return JSON dalam urutan yang benar
+
+# flask swagger configs
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.yaml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Sentiment"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+
+# Homepage
+@app.route('/', methods=['GET'])
+def get():
+    return "WELCOME" 
+
+
+@app.route('/RNN/text', methods=["POST"])
+def RNN_text():
+    if request.method == "POST":
+        input_text = str(request.form["text"])
+        sentiment = sentiment_text(input_text,'RNN')
+        output = dict(input=input_text, sentiment=sentiment)
+        return jsonify(output)
+
+@app.route('/RNN/file', methods=["POST"])
+def RNN_file():
+    if request.method == "POST":
+        file = request.files['file']
+
+        try:
+            file = pd.read_csv(file, encoding='iso-8859-1')
+        except:
+            try:
+                file = pd.read_csv(file, encoding='utf-8')
+            except:
+                try:
+                    file = pd.read_csv(file, sep='\t')
+                except:
+                    pass
+        print("======== read data csv to pandas =========")
+        print(type(file))
+        if(isinstance(file, pd.DataFrame)):
+            file = sentiment_file(file,'RNN')
+            if(isinstance(file, pd.DataFrame)):
+                response = Response(file.to_json(orient="records"), mimetype='application/json')
+            else:
+                response = "Error"
+        else:
+            response = "Error"
+        return response
+
+
+
+
+
+@app.route('/CNN/text', methods=["POST"])
+def CNN_text():
+    if request.method == "POST":
+        input_text = str(request.form["text"])
+        sentiment = sentiment_text(input_text,'CNN')
+        output = dict(input=input_text, sentiment=sentiment)
+        return jsonify(output)
+
+@app.route('/CNN/file', methods=["POST"])
+def CNN_file():
+    if request.method == "POST":
+        file = request.files['file']
+
+        try:
+            file = pd.read_csv(file, encoding='iso-8859-1')
+        except:
+            try:
+                file = pd.read_csv(file, encoding='utf-8')
+            except:
+                try:
+                    file = pd.read_csv(file, sep='\t')
+                except:
+                    pass
+        print("======== read data csv to pandas =========")
+        print(type(file))
+        if(isinstance(file, pd.DataFrame)):
+            file = sentiment_file(file,'CNN')
+            if(isinstance(file, pd.DataFrame)):
+                response = Response(file.to_json(orient="records"), mimetype='application/json')
+            else:
+                response = "Error"
+        else:
+            response = "Error"
+        return response
+
+
+
+
+
+@app.route('/LSTM/text', methods=["POST"])
+def LSTM_text():
+    if request.method == "POST":
+        input_text = str(request.form["text"])
+        sentiment = sentiment_text(input_text,'lstm')
+        output = dict(input=input_text, sentiment=sentiment)
+        return jsonify(output)
+
+@app.route('/LSTM/file', methods=["POST"])
+def LSTM_file():
+    if request.method == "POST":
+        file = request.files['file']
+
+        try:
+            file = pd.read_csv(file, encoding='iso-8859-1')
+        except:
+            try:
+                file = pd.read_csv(file, encoding='utf-8')
+            except:
+                try:
+                    file = pd.read_csv(file, sep='\t')
+                except:
+                    pass
+        print("======== read data csv to pandas =========")
+        print(type(file))
+        if(isinstance(file, pd.DataFrame)):
+            file = sentiment_file(file,'LSTM')
+            if(isinstance(file, pd.DataFrame)):
+                response = Response(file.to_json(orient="records"), mimetype='application/json')
+            else:
+                response = "Error"
+        else:
+            response = "Error"
+        return response
+
+
+
+# error handling
+@app.errorhandler(400)
+def handle_400_error(_error):
+    "Return a http 400 error to client"
+    return make_response(jsonify({'error': 'Misunderstood'}), 400)
+
+
+@app.errorhandler(401)
+def handle_401_error(_error):
+    "Return a http 401 error to client"
+    return make_response(jsonify({'error': 'Unauthorised'}), 401)
+
+
+@app.errorhandler(404)
+def handle_404_error(_error):
+    "Return a http 404 error to client"
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(500)
+def handle_500_error(_error):
+    "Return a http 500 error to client"
+    return make_response(jsonify({'error': 'Server error'}), 500)
+
+
+#Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
+# Default IP 127.0.0.1 Port 5000
+
+
+
+
+
+
+
+
---
+++ API_code/sentimentprediction.py
@@ -0,0 +1,153 @@
+from keras.models import load_model
+import re 
+import pickle 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.models import load_model
+
+
+df= pd.read_csv('train_preprocess.tsv', sep='\t', header=None)
+df.columns =['text', 'label']
+
+def lowercase(text):
+    return text.lower()
+
+def remove_unnecessary_char(text):
+    text = re.sub('\n',' ',text) # Remove every '\n'
+    text = re.sub('rt',' ',text) # Remove every retweet symbol
+    text = re.sub('user',' ',text) # Remove every username
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ',text) # Remove every URL
+    text = re.sub('  +', ' ', text) # Remove extra spaces
+    return text
+    
+def remove_nonaplhanumeric(text):
+    text = re.sub('[^0-9a-zA-Z]+', ' ', text) 
+    return text
+
+def preprocess(text):
+    text = lowercase(text) # 1
+    text = remove_nonaplhanumeric(text) # 2
+    text = remove_unnecessary_char(text) # 3
+    return text
+
+df['text_clean'] = df.text.apply(preprocess)
+
+neg = df.loc[df['label'] == 'negative'].text_clean.tolist()
+neu = df.loc[df['label'] == 'neutral'].text_clean.tolist()
+pos = df.loc[df['label'] == 'positive'].text_clean.tolist()
+
+neg_label = df.loc[df['label'] == 'negative'].label.tolist()
+neu_label = df.loc[df['label'] == 'neutral'].label.tolist()
+pos_label = df.loc[df['label'] == 'positive'].label.tolist()
+
+total_data = pos + neu + neg
+labels = pos_label + neu_label + neg_label
+
+max_features = 100000
+tokenizer = Tokenizer(num_words=max_features, split=' ', lower=True)
+tokenizer.fit_on_texts(total_data)
+
+X = tokenizer.texts_to_sequences(total_data)
+
+vocab_size = len(tokenizer.word_index)
+maxlen = max(len(x) for x in X)
+
+X = pad_sequences(X)
+Y = pd.get_dummies(labels)
+Y = Y.values
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+embed_dim = 100
+units = 64
+
+# model = Sequential()
+# model.add(Embedding(max_features, embed_dim, input_length=X.shape[1]))
+# model.add(LSTM(units, dropout=0.2))
+# model.add(Dense(3, activation='softmax'))
+# model.compile(loss = 'binary_crossentropy', optimizer='adam', metrics = ['accuracy'])
+# print(model.summary())
+
+# adam = optimizers.Adam(learning_rate = 0.001)
+# model.compile(loss = 'categorical_crossentropy', optimizer = adam, metrics = ['accuracy'])
+
+#es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
+# history = model.fit(X_train, y_train, epochs=10, batch_size=10, validation_data=(X_test, y_test), verbose=1)
+
+# predictions = model.predict(X_test)
+# y_pred = predictions
+# matrix_test = metrics.classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+
+input_text = """ 
+beta sedang berjuang melawan kanker
+"""
+
+sentiment = ['negative', 'neutral', 'positive']
+model = load_model('LSTM.h5')
+
+
+
+def sentiment_text(input_text, model):
+    if(model =="lstm"):
+        model = load_model('LSTM.h5')
+    elif(model == "RNN"):
+        model = load_model('RNN.h5')
+    elif(model == "CNN"):
+        model = load_model('CNN.h5')
+
+    text = [preprocess(input_text)]
+    predicted = tokenizer.texts_to_sequences(text)
+    guess = pad_sequences(predicted, maxlen=X.shape[1])
+
+    prediction = model.predict(guess)
+    print(prediction[0])
+    polarity = np.argmax(prediction[0])
+
+    print('Prediction: ',prediction)
+    print('Polarity :',polarity)
+    print('Text: ',text[0])
+    print('Sentiment: ',sentiment[polarity])
+
+    return sentiment[polarity]
+
+
+
+
+def sentiment_file(file, model):
+    if(model =="lstm"):
+        model = load_model('LSTM.h5')
+    elif(model == "RNN"):
+        model = load_model('RNN.h5')
+    elif(model == "CNN"):
+        model = load_model('CNN.h5')
+    first_column = file.iloc[:, 0]
+    file = first_column.astype("string").apply(preprocess)
+    print("======== finish preprocess =========")
+
+    file = file.to_frame()
+    if(isinstance(file, pd.DataFrame)):
+        file.rename(columns={ file.columns[0]: "Tweet" }, inplace = True)
+        file["Sentiment"] = None
+        file['Tweet'] = file['Tweet'].astype('string')
+        file['Sentiment'] = file['Sentiment'].astype('string')
+
+        for i in range(len(file)):
+            text = file['Tweet'][i]
+            text = [text]
+
+            predicted = tokenizer.texts_to_sequences(text)
+            guess = pad_sequences(predicted, maxlen=X.shape[1])
+
+            prediction = model.predict(guess)
+
+            polarity = np.argmax(prediction[0])
+
+            file["Sentiment"][i] =  sentiment[polarity]
+
+        print("======== FINISH TEST =========")
+        return file
+    else:
+        print("======== FAILED TEST =========")
+        return "File is Unreadable"
